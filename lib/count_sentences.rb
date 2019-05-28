0e5d1ec5a2de816c93ffa269122082a89e4ba078
Adella <adellakenn@gmail.com>
2019-05-28 23:52:11 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # Use Regex 
+    # scan: for each match, a result is generated and either added to the result array
+    # must have [] + [] because it is for one or more of [.!?]
+    self.scan(/[^\.!?]+[\.!?]/).count
+    
   end
 end
