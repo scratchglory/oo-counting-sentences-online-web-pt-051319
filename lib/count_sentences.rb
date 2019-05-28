36340e789d4ce26fe69c21d805d431c4fc374e0a
Adella <adellakenn@gmail.com>
2019-05-28 23:45:34 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -18,6 +18,6 @@ class String
     #=> scan has regex to split string and strip will remove trailing spaces.
     # strip eturns a copy of str with leading and trailing whitespace removed.
     # self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
-    self.scan(/[^\.!?]+[\.!?]/).map.strip.count
+    
   end
 end
